2a8e00fd6a9775c2a0aa607371ebb2c46d105a70
James Mann <james.mann@bcs.org>
2018-08-06 21:47:51 +0100

    demo functionally complete now

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -6,21 +6,14 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AzureAdAuth.Controllers
 {
-    public class ValuesController : Controller
+    public class AuthController : Controller
     {
         public AzureAdAuthorizationCodeFlow Oauth { get; }
 
-        public ValuesController(AzureAdAuthorizationCodeFlow oauth)
+        public AuthController(AzureAdAuthorizationCodeFlow oauth)
         {
             Oauth = oauth;
         }
-
-        //[HttpGet("adminconsentredirect")]
-        //public ContentResult GetAdminConsent(string state)
-        //{
-        //    Oauth.AdminConsented(state);
-        //    return Content("Thanks for providing admin consent, you may now close this window.");
-        //}
 
         [HttpGet("userconsentredirect")]
         public ContentResult GetUserConsent(string code, string state)
--- Controllers/AzureAdAuthorizationCodeFlow.cs
+++ Controllers/AzureAdAuthorizationCodeFlow.cs
@@ -1,9 +1,8 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.Graph;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Dynamic;
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -13,32 +12,19 @@ namespace AzureAdAuth.Controllers
     {
         string azureAdTenant = "jemann.onmicrosoft.com";
         string clientId = "7acef7ed-25a7-4a41-9156-be2b701c789c";
-        string adminConsentRedirectUri = "https://localhost:44355/adminconsentredirect";
         string userConsentRedirectUri = "https://localhost:44355/userconsentredirect";
-        string permissionsRequested = "User.Read.All";
+        string permissionsRequested = "Calendars.ReadWrite.Shared";
         string clientSecret = "";
 
         public AzureAdAuthorizationCodeFlow()
         {
             Console.WriteLine("------------------------------------");
             Console.WriteLine("------------------------------------");
-            Console.WriteLine("(LOG IN WITH AN TENANT ADMIN) Provide admin consent at:");
-            //Console.WriteLine($"https://login.microsoftonline.com/{azureAdTenant}/adminconsent?client_id={clientId}&state=12345&redirect_uri={adminConsentRedirectUri}");
+            Console.WriteLine(" Provide consent at:");
             Console.WriteLine($"https://login.microsoftonline.com/{azureAdTenant}/oauth2/v2.0/authorize?client_id={clientId}&scope={permissionsRequested}&response_type=code&response_mode=query&redirect_uri={userConsentRedirectUri}&state=12345");
             Console.WriteLine("------------------------------------");
             Console.WriteLine("------------------------------------");
         }
-
-        //public void AdminConsented(string state)
-        //{
-        //    Console.WriteLine("------------------------------------");
-        //    Console.WriteLine("------------------------------------");
-        //    Console.WriteLine("*** ADMIN CONSENTED ***");
-        //    Console.WriteLine("(LOG IN WITH ANY USER IN THE TENANT) Provide user consent at:");
-        //    Console.WriteLine($"https://login.microsoftonline.com/{azureAdTenant}/oauth2/v2.0/authorize?client_id={clientId}&scope={permissionsRequested}&response_type=code&response_mode=query&redirect_uri={userConsentRedirectUri}&state={state}");
-        //    Console.WriteLine("------------------------------------");
-        //    Console.WriteLine("------------------------------------");
-        //}
 
         public async Task UserConsented(string code, string state)
         {
@@ -46,11 +32,12 @@ namespace AzureAdAuth.Controllers
             Console.WriteLine("------------------------------------");
             Console.WriteLine("*** USER CONSENTED ***");
 
-            // execute a query that requires admin consent (my manager)
             var httpClient = new HttpClient();
+            // get an access token from azure ad
             var accessToken = await httpClient.GetAzureAdToken(azureAdTenant, code, clientId, userConsentRedirectUri, clientSecret, permissionsRequested);
 
-            (var name, var phone, var email) = await httpClient.GetMicrosoftGraphMyManager(accessToken);
+            // find 1 hour meeting slots in the next 24 hours in room 
+            var meetingTimes = await httpClient.GetMicrosoftGraphFindMeeting(accessToken, DateTime.Now, DateTime.Now.AddDays(1), "PT1H", "boardroom@jemann.onmicrosoft.com");
 
             Console.WriteLine(code);
             Console.WriteLine("------------------------------------");
@@ -60,6 +47,7 @@ namespace AzureAdAuth.Controllers
 
     public static class AzureAdExtensions
     {
+        // This method retrieves an azure ad access token after the user has consented and a authorization code has been provided
         public static async Task<string> GetAzureAdToken(this HttpClient client, string tenant, string code, string clientId, string redirectUri, string clientSecret, string permissionsRequested)
         {
             var formFields = new List<KeyValuePair<string, string>>
@@ -80,18 +68,47 @@ namespace AzureAdAuth.Controllers
 
     public static class MicrosoftGraphExtensions
     {
-        public static async Task<(string name, string phone, string email)> GetMicrosoftGraphMyManager(this HttpClient client, string accessToken)
+        // Find meeting times in graph
+        public static async Task<MeetingTimeSuggestionsResult> GetMicrosoftGraphFindMeeting(this HttpClient client, string accessToken, DateTime from, DateTime to, string meetingDuration, string meetingRoomEmailAddress)
         {
-            dynamic result = await ExecuteGraphQuery(client, accessToken, "https://graph.microsoft.com/v1.0/me/manager");
-            return (result.displayName, result.mobilePhone, result.mail);
+            var graphClient = new GraphServiceClient(new PreAuthorizedBearerTokenAuthenticationProvider(accessToken));
+            return await graphClient.Me.FindMeetingTimes(
+                LocationConstraint: new LocationConstraint()
+                {
+                    IsRequired = true,
+                    Locations = new LocationConstraintItem[] 
+                    {
+                        new LocationConstraintItem() { LocationEmailAddress = meetingRoomEmailAddress }
+                    }
+                }, 
+                TimeConstraint: new TimeConstraint()
+                {
+                    ActivityDomain = ActivityDomain.Unrestricted,
+                    Timeslots = new TimeSlot[] {
+                        new TimeSlot()
+                        {
+                            Start = new DateTimeTimeZone() { DateTime = from.ToString("yyyy-MM-ddThh:mm:ss"), TimeZone = "UTC" },
+                            End = new DateTimeTimeZone() { DateTime = to.ToString("yyyy-MM-ddThh:mm:ss"), TimeZone = "UTC" }
+                        }
+                    }
+                },
+                MeetingDuration: new Duration(meetingDuration)).Request().PostAsync();
         }
-        private static async Task<dynamic> ExecuteGraphQuery(HttpClient client, string accessToken, string graphUrl)
+
+        // Authenticate using our access token
+        class PreAuthorizedBearerTokenAuthenticationProvider : IAuthenticationProvider
         {
-            var graphreq = new HttpRequestMessage(HttpMethod.Get, graphUrl);
-            graphreq.Headers.Add("Authorization", $"Bearer {accessToken}");
-            var graphres = await client.SendAsync(graphreq);
-            dynamic result = JsonConvert.DeserializeObject<ExpandoObject>(await graphres.Content.ReadAsStringAsync());
-            return result;
+            public PreAuthorizedBearerTokenAuthenticationProvider(string accessToken)
+            {
+                AccessToken = accessToken;
+            }
+
+            public string AccessToken { get; }
+
+            public async Task AuthenticateRequestAsync(HttpRequestMessage request)
+            {
+                request.Headers.Add("Authorization", $"Bearer {AccessToken}");
+            }
         }
     }
 }
